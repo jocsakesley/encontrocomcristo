8e9bbf41f6d80f2ac15fc4240ff4b108957d5cd6
jocsakesley <jocsadm@gmail.com>
2021-02-04 14:33:06 -0300

    Criando equipes

--- equipes/api/serializers.py
+++ equipes/api/serializers.py
@@ -1,9 +1,48 @@
 from rest_framework import serializers
 from equipes.models import Equipe
+from funcao.models import Funcao
 from participantes.api.serializers import ParticipantesSerializer
+from participantes.models import Participante
+
 class EquipesSerializer(serializers.ModelSerializer):
     lider = ParticipantesSerializer()
     membros = ParticipantesSerializer(many=True)
     class Meta:
         model = Equipe
         fields = "__all__"
+
+    def cria_membros(self, membros, equipes):
+        for membro in membros:
+            funcao = membro['funcao']
+            del membro['funcao']
+
+            func = Funcao.objects.get_or_create(**funcao)[0]
+            membro['funcao'] = func
+            mb = Participante.objects.get_or_create(**membro)[0]
+            print(mb)
+            equipes.membros.add(mb)
+
+    def create(self, validated_data):
+        print(dict(validated_data['lider']))
+        lider = validated_data['lider']
+        del validated_data['lider']
+        membros = validated_data['membros']
+        del validated_data['membros']
+
+
+        equipes = Equipe.objects.create(**validated_data)
+        self.cria_membros(membros, equipes)
+        funcao = dict(lider)
+        funcao = dict(funcao['funcao'])
+        print(funcao)
+
+        func = Funcao.objects.get_or_create(**funcao)[0]
+        lider = dict(lider)
+        lider['funcao'] = func
+
+        lid = Participante.objects.get_or_create(**lider)[0]
+        equipes.lider = lid
+        equipes.save()
+        return equipes
+
+    #def update(self, instance, validated_data):
--- equipes/models.py
+++ equipes/models.py
@@ -4,8 +4,8 @@ from participantes.models import Participante
 class Equipe(models.Model):
     nome = models.CharField(max_length=150)
     descricao = models.TextField()
-    lider = models.ForeignKey(Participante, on_delete=models.DO_NOTHING, related_name="lider_equipe")
-    membros = models.ManyToManyField(Participante, related_name="membros_equipe")
+    lider = models.ForeignKey(Participante, on_delete=models.DO_NOTHING, related_name="lider_equipe", null=True)
+    membros = models.ManyToManyField(Participante, related_name="membros_equipe", null=True)
 
     def __str__(self):
         return self.nome
